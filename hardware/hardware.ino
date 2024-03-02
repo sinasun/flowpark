fc87d5ef1346b0e1f0fe64d1980dbc450468b11c
sina-negarandeh <negarandeh.sina@gmail.com>
2024-03-02 13:56:13 -0500

    Add status to hardware sensor

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -4,7 +4,14 @@
 const char* ssid = "sina";
 const char* password = "haha2020hehe";
 const char* host = "192.168.205.16"; // Change this to your server's IP address
-const int port = 3000;
+const int port = 5000;
+
+WiFiClient client;
+
+const char* site = "parking1";
+const int slot = 2;
+
+bool previous_status = false;
 
 void setup() {
   Serial.begin(9600);
@@ -27,26 +34,27 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  connect(client);
 }
 
-void loop() {
-  // Send POST request
-  WiFiClient client;
+float get_distance() {
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue * (3.3 / 1023.0);
+  float inches = voltage * 51.8;
 
-  Serial.print("Connecting to ");
-  Serial.print(host);
-  Serial.print(":");
-  Serial.println(port);
+  Serial.print("Distance: ");
+  Serial.print(inches);
+  Serial.println(" inches");
 
-  if (!client.connect(host, port)) {
-    Serial.println("Connection failed!");
-    return;
-  }
+  delay(1000);
+}
 
+void send_message(bool status) {
   // Construct HTTP POST request
-  String url = "/sendPostRequest";
-  String postData = "{\"key\": \"value\"}";
-  
+  String url = "/publish";
+  String postData = "{\"topic\": \"" + String(site) + "\", \"message\": " + "\"" + String(slot) + ":" + String(status) + "\"}";
+
   Serial.print("Requesting URL: ");
   Serial.println(url);
 
@@ -59,6 +67,35 @@ void loop() {
   client.print(postData);
 
   Serial.println("Request sent");
+}
+
+void connect() {
+  Serial.print("Connecting to ");
+  Serial.print(host);
+  Serial.print(":");
+  Serial.println(port);
+
+  while (!client.connect(host, port)) {
+    Serial.println("Connection failed!");
+  }
+}
+
+bool check_parking_slot (float threshold = 10.0) {
+  distance = get_distance()
+  if (previous_status == true && threshold < distance) {
+    return true;
+  } else (previous_status == false && distance <= threshold) {
+    return true;
+  }
+  return false;
+}
+
+void loop() {
+  if (check_parking_slot()) {
+    send_message(!previous_status);
+    previous_status = !previous_status;
+  }
+
   delay(5000); // Send request every 5 seconds
 }
 
