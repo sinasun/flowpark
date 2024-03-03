8d56cdd0ea0b3f631b5c9b7d013467c45365799f
king <kingbian72@gmail.com>
2024-03-03 02:04:13 -0500

    implement solution components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,83 +1,93 @@
-'use client';
-import { Link } from '@nextui-org/link';
-import { button as buttonStyles } from '@nextui-org/theme';
-import { siteConfig } from '@/config/site';
-import { GithubIcon } from '@/components/icons';
-import { Button } from '@nextui-org/button';
-import { signIn, useSession } from 'next-auth/react';
-import About from '@/components/about';
-import Statement from '@/components/statement';
-import { TypewriterEffectSmooth } from '@/components/type-writer';
-import { useRouter } from 'next/navigation';
+"use client";
+import { Link } from "@nextui-org/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { GithubIcon } from "@/components/icons";
+import { Button } from "@nextui-org/button";
+import { signIn, useSession } from "next-auth/react";
+import About from "@/components/about";
+import Statement from "@/components/statement";
+import Solution from "@/components/solution";
+import { TypewriterEffectSmooth } from "@/components/type-writer";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-	const { data: session } = useSession();
-	const user = session && session.user;
-	const words = [
-		{
-			text: 'Find',
-		},
-		{
-			text: 'Parking',
-		},
-		{
-			text: 'Spot',
-		},
-		{
-			text: 'Around',
-		},
-		{
-			text: 'You in',
-		},
-		{
-			text: 'Real Time',
-			className: 'text-blue-500 dark:text-blue-500',
-		},
-	];
+  const { data: session } = useSession();
+  const user = session && session.user;
+  const words = [
+    {
+      text: "Imagine",
+    },
+    {
+      text: "a",
+    },
+    {
+      text: "Society",
+    },
+    {
+      text: "Where",
+    },
+    {
+      text: "Finding",
+    },
+    {
+      text: "Parking",
+    },
+    {
+      text: "is",
+    },
+    {
+      text: "Effortless!",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  const router = useRouter();
 
-	const router = useRouter();
+  const handleButtonClick = async () => {
+    if (session?.user) {
+      router.push("/dashboard");
+    } else {
+      await signIn("auth0", {
+        callbackUrl: "/dashboard",
+      });
+    }
+  };
 
-	const handleButtonClick = async () => {
-		if (session?.user) {
-			router.push('/dashboard');
-		} else {
-			await signIn('auth0', {
-				callbackUrl: '/dashboard',
-			});
-		}
-	};
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="flex flex-col text-center justify-center">
+        <TypewriterEffectSmooth words={words} />
+        <p className="mt-4">
+          <strong>Park Spotter</strong> provides real-time data about parking
+          spot around you.
+        </p>
+      </div>
 
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="flex flex-col text-center justify-center">
-				<TypewriterEffectSmooth words={words} />
-				<p className="mt-4">
-					<strong>Park Spotter</strong> provides real-time data about parking
-					spot around you.
-				</p>
-			</div>
+      <div className="flex gap-3 mt-12">
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+        <Button color="primary" onClick={handleButtonClick} variant="flat">
+          Access the App
+        </Button>
+      </div>
 
-			<div className="flex gap-3 mt-12">
-				<Link
-					isExternal
-					className={buttonStyles({ variant: 'bordered', radius: 'full' })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-				<Button color="primary" onClick={handleButtonClick} variant="flat">
-					Access the App
-				</Button>
-			</div>
+      <div className=" flex flex-row ml-auto">
+        <About />
+      </div>
 
-			<div className=" flex flex-row ml-auto">
-				<About />
-			</div>
+      <div>
+        <Statement />
+      </div>
 
-			<div>
-				<Statement />
-			</div>
-		</section>
-	);
+      <div>
+        <Solution />
+      </div>
+    </section>
+  );
 }
--- frontend/components/solution.tsx
+++ frontend/components/solution.tsx
@@ -1,39 +1,46 @@
 import React from "react";
 
-const Statement = () => {
+const Solution = () => {
   return (
     <div className=" mt-10 flex flex-col items-center">
-      <h1 className=" mt-8 text-[50px] font-bold mb-8">
-        {" "}
-        The Problem statement
-      </h1>
+      <h1 className=" mt-8 text-[50px] font-bold mb-8"> The Solution</h1>
       <p className="text-[18px] mb-4">
-        With the global car population projected to increase significantly (with
-        88.5 million units expected in 2024), busy cities and event organizers
-        face a growing challenge. Finding a parking spot can be a frustrating,
-        time-consuming experience, not just for drivers, but also for the
-        environment. Current parking solutions often lack real-time data and
-        efficient allocation, leading to:
+        The proposed applications works by using advance low latency sonar rang
+        detection systems to assist cities, organizers, and drivers tackle the
+        rising problem of parking in our busy society offering a cost effective
+        implementation over the competition.
       </p>
-      <ul className="text-[18px]">
-        <li className="mb-4">
-          <span className="font-bold">Wasted time and frustration:</span>{" "}
-          Drivers spend precious time searching for parking, increasing stress
-          and potentially impacting their schedules.
+      <ul className="text-[18px] mb-4">
+        <li>Utilizes advanced low-latency sonar range detection systems</li>
+        <li>
+          Assists cities, organizers, and drivers in addressing parking
+          challenges
         </li>
-        <li className="mb-4">
-          <span className="font-bold">Traffic congestion:</span> Circles
-          searching for parking contribute to traffic jams, further increasing
-          frustration and pollution.
-        </li>
-        <li className="mb-4">
-          <span className="font-bold">Increased emissions:</span> Idling
-          vehicles searching for parking contribute to unnecessary emissions,
-          harming the environment.
-        </li>
+        <li>Provides real-time parking information</li>
+        <li>Offers a cost-effective solution compared to competitors</li>
       </ul>
+
+      {/*
+     
+      this is an impact:
+
+      * Improved Efficiency
+        Real-time data: By using a cloud-based sonar system, real-time data can be sent to users in order to quickly and effectively direct them to parking spaces that are available.
+        Users can obtain precise and current information by utilizing innovative technology such as
+        event driven architecture, which cuts down on the time spent looking for parking and increases efficiency overall.
+
+      *Ease of installation
+        The sonar system's cloud-based infrastructure simplifies setup 
+        and provides users with an easier installation experience. Because of how simple 
+        it is to implement, businesses may effectively streamline this operations.
+
+
+      *Evironmental effective
+        By cutting down on the amount of time needed to find parking, FlowPark,  reduces vehicle emissions and carbon footprint. This maximizes the use of parking resources while 
+        providing users with substantial financial rewards that are in line with sustainability goals.
+      By unitizing cloud base architecture   */}
     </div>
   );
 };
 
-export default Statement;
+export default Solution;
