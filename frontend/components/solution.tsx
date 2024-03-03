f9dea23337df63e14a4e9a64b695bd90aacf1b92
sinasun <sinakhodaveisi@gmail.com>
2024-03-03 02:10:19 -0500

    build stuff

--- frontend/components/solution.tsx
+++ frontend/components/solution.tsx
@@ -1,45 +1,41 @@
-import React from "react";
+export const Solution = () => {
+	return (
+		<div className=" mt-10 flex flex-col items-center">
+			<h1 className=" mt-8 text-[50px] font-bold mb-8"> The Solution</h1>
+			<p className="text-[18px] mb-4">
+				The proposed applications works by using advance low latency sonar rang
+				detection systems to assist cities, organizers, and drivers tackle the
+				rising problem of parking in our busy society offering a cost effective
+				implementation over the competition.
+			</p>
+			<ul className="text-[18px] mb-4">
+				<li>Utilizes advanced low-latency sonar range detection systems</li>
+				<li>
+					Assists cities, organizers, and drivers in addressing parking
+					challenges
+				</li>
+			</ul>
+			<ul className="text-[18px]">
+				<li className="mb-4">
+					<span className="font-bold">Wasted time and frustration:</span>{' '}
+					Drivers spend precious time searching for parking, increasing stress
+					and potentially impacting their schedules.
+				</li>
+				<li className="mb-4">
+					<span className="font-bold">Traffic congestion:</span> Circles
+					searching for parking contribute to traffic jams, further increasing
+					frustration and pollution.
+				</li>
+				<li className="mb-4">
+					<span className="font-bold">Increased emissions:</span> Idling
+					vehicles searching for parking contribute to unnecessary emissions,
+					harming the environment.
+				</li>
+				<li>Provides real-time parking information</li>
+				<li>Offers a cost-effective solution compared to competitors</li>
+			</ul>
 
-const Solution = () => {
-  return (
-    <div className=" mt-10 flex flex-col items-center">
-      <h1 className=" mt-8 text-[50px] font-bold mb-8"> The Solution</h1>
-      <p className="text-[18px] mb-4">
-        The proposed applications works by using advance low latency sonar rang
-        detection systems to assist cities, organizers, and drivers tackle the
-        rising problem of parking in our busy society offering a cost effective
-        implementation over the competition.
-      </p>
-<<<<<<< HEAD
-      <ul className="text-[18px] mb-4">
-        <li>Utilizes advanced low-latency sonar range detection systems</li>
-        <li>
-          Assists cities, organizers, and drivers in addressing parking
-          challenges
-=======
-      <ul className="text-[18px]">
-        <li className="mb-4">
-          <span className="font-bold">Wasted time and frustration:</span>{" "}
-          Drivers spend precious time searching for parking, increasing stress
-          and potentially impacting their schedules.
-        </li>
-        <li className="mb-4">
-          <span className="font-bold">Traffic congestion:</span> Circles
-          searching for parking contribute to traffic jams, further increasing
-          frustration and pollution.
-          
-        </li>
-        <li className="mb-4">
-          <span className="font-bold">Increased emissions:</span> Idling
-          vehicles searching for parking contribute to unnecessary emissions,
-          harming the environment.
->>>>>>> 2b4e37af61e840378d5a8a832eb10efbde78612c
-        </li>
-        <li>Provides real-time parking information</li>
-        <li>Offers a cost-effective solution compared to competitors</li>
-      </ul>
-
-      {/*
+			{/*
      
       this is an impact:
 
@@ -58,8 +54,8 @@ const Solution = () => {
         By cutting down on the amount of time needed to find parking, FlowPark,  reduces vehicle emissions and carbon footprint. This maximizes the use of parking resources while 
         providing users with substantial financial rewards that are in line with sustainability goals.
       By unitizing cloud base architecture   */}
-    </div>
-  );
+		</div>
+	);
 };
 
 export default Solution;
