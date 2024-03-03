a95bc44ec6836ae57b01b31bd55cfcb993074634
sinasun <sinakhodaveisi@gmail.com>
2024-03-03 03:33:36 -0500

    database upgrades

---
+++ frontend/seed.js
@@ -0,0 +1,63 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+	await prisma.parking.createMany({
+		data: [
+			{
+				name: 'parking1',
+				position1: 1,
+				position2: 1,
+				position3: 1,
+				position4: 1,
+				position5: 1,
+				position6: 1,
+				position7: 1,
+				position8: 1,
+			},
+			{
+				name: 'parking2',
+				position1: 1,
+				position2: 1,
+				position3: 1,
+				position4: 1,
+				position5: 1,
+				position6: 1,
+				position7: 1,
+				position8: 1,
+			},
+			{
+				name: 'parking3',
+				position1: 1,
+				position2: 1,
+				position3: 1,
+				position4: 1,
+				position5: 1,
+				position6: 1,
+				position7: 1,
+				position8: 1,
+			},
+			{
+				name: 'parking4',
+				position1: 1,
+				position2: 1,
+				position3: 1,
+				position4: 1,
+				position5: 1,
+				position6: 1,
+				position7: 1,
+				position8: 1,
+			},
+		],
+	});
+}
+
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
